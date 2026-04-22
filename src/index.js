ccabe5ea9d71d688d6d95fceaa0a06a2f7525e16
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-22 15:42:49 -0400

    chore: import and manually test createTodoList module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // imports
 import initlocalStorageData from "./onPageLoad.js";
 import createTodoItem from "./crud-ops/createTodoItem.js";
+import createTodoList from "./crud-ops/createTodoList.js";
 
 // check for and initialize app data upon complete page load
 window.addEventListener("load", initlocalStorageData);
 
 // manual testing
 // console.log(createTodoItem(["kitty", "a meowing cat", "2026-04-17", "high", "defaultList"]));
+// console.log(createTodoList(["turkish van toys", "meow, meow, meow! more catnip, please!"]));
