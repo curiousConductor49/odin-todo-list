bcc9b65114d02ed7f63b55782904b2411f231bf5
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-22 15:58:39 -0400

    chore: import and manually test addTodoItemToStorage module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import initlocalStorageData from "./onPageLoad.js";
 import createTodoItem from "./crud-ops/createTodoItem.js";
 import createTodoList from "./crud-ops/createTodoList.js";
+import addTodoItemToStorage from "./crud-ops/addTodoItemToStorage.js";
 
 // check for and initialize app data upon complete page load
 window.addEventListener("load", initlocalStorageData);
@@ -9,3 +10,7 @@ window.addEventListener("load", initlocalStorageData);
 // manual testing
 // console.log(createTodoItem(["kitty", "a meowing cat", "2026-04-17", "high", "defaultList"]));
 // console.log(createTodoList(["turkish van toys", "meow, meow, meow! more catnip, please!"]));
+// const item = createTodoItem(["kitty", "a meowing cat", "2026-04-17", "high", "defaultList"]);
+// const item2 = createTodoItem(["calico", "a frowning cat", "2026-08-21", "medium", "defaultList"]);
+// addTodoItemToStorage(item);
+// addTodoItemToStorage(item2);
