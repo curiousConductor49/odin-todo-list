05a2019580407dbcdc816828d84152b55237b2a8
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-22 15:31:15 -0400

    chore: import createTodoItem module and do some manual testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-// manual testing
+// imports
 import initlocalStorageData from "./onPageLoad.js";
+import createTodoItem from "./crud-ops/createTodoItem.js";
 
 // check for and initialize app data upon complete page load
 window.addEventListener("load", initlocalStorageData);
+
+// manual testing
+// console.log(createTodoItem(["kitty", "a meowing cat", "2026-04-17", "high", "defaultList"]));
