ca6473e4708b032ce3f72450bcf29ff3e281cf89
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-23 12:37:34 -0400

    feat: update module to include parent todo list title and id as object properties

--- src/crud-ops/createTodoItem.js
+++ src/crud-ops/createTodoItem.js
@@ -1,6 +1,9 @@
 export default function createTodoItem(inputData) {
     // destructure input data
     const [ title, description, dueDate, priority, parentList ] = inputData;
+    // get JSON data for todo item's parent list id
+    const todoLists = JSON.parse(localStorage.getItem("todoAppData"))["todoLists"];
+    const parentTodoListId = todoLists.find(list => list.title === parentList)["id"];
     // create and return todo item object
     const todoItem = {
         title,
@@ -8,7 +11,8 @@ export default function createTodoItem(inputData) {
         description,
         dueDate: new Date(dueDate).toISOString(),
         priority,
-        parentList,
+        parentListTitle: parentList,
+        parentListId: parentTodoListId, 
     }
     return todoItem;
 }
