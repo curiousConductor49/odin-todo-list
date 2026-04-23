042d18579b9a2047950a241e70da1bdeb1586e2c
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-23 12:37:59 -0400

    chore: import and manually test module to delete todo lists from localStorage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import createTodoList from "./crud-ops/createTodoList.js";
 import addTodoItemToStorage from "./crud-ops/addTodoItemToStorage.js";
 import addTodoListToStorage from "./crud-ops/addTodoListToStorage.js";
 import deleteTodoItemFromStorage from "./crud-ops/deleteTodoItemFromStorage.js";
+import deleteTodoListFromStorage from "./crud-ops/deleteTodoListFromStorage.js";
 
 // check for and initialize app data upon complete page load
 window.addEventListener("load", initlocalStorageData);
@@ -23,11 +24,27 @@ window.addEventListener("load", initlocalStorageData);
 // const list = createTodoList(["tabby cat troubles", "pspspsps! there's not enough laser pens!"]);
 // addTodoListToStorage(list);
 
+// NOTE: when manually testing deleting a todo item/list, be sure to first comment out the code to create and add new items/lists, else the program will delete the specified item/list, then add new items/lists (upon refresh to update localStorage, the program runs again)
+
 // DELETING TODO ITEMS
 // const item = createTodoItem(["kitty", "a meowing cat", "2026-04-17", "high", "defaultList"]);
 // const item2 = createTodoItem(["calico", "a frowning cat", "2026-08-21", "medium", "defaultList"]);
 // addTodoItemToStorage(item);
 // addTodoItemToStorage(item2);
 // deleteTodoItemFromStorage("");
-// NOTE: when manually testing deleting a todo item, be sure to first comment out the code to create and add new items, else the program will delete the specified item, then add new items (upon refresh to update localStorage, the program runs again)
 
+// DELETING TODO LISTS
+// add new lists
+// const list = createTodoList(["tabby troubles", "..."]);
+// const list2 = createTodoList(["bengal biscuits", "..."]);
+// addTodoListToStorage(list);
+// addTodoListToStorage(list2);
+// // add new items
+// const item = createTodoItem(["tortoiseshell", "a sleeping cat", "2026-04-17", "high", "tabby troubles"]);
+// const item2 = createTodoItem(["calico", "a frowning cat", "2026-08-21", "medium", "tabby troubles"]);
+// const item3 = createTodoItem(["bengal", "a meowing cat", "2026-09-14", "medium", "bengal biscuits"]);
+// addTodoItemToStorage(item);
+// addTodoItemToStorage(item2);
+// addTodoItemToStorage(item3);
+// delete a list (should delete the list + the todo items under it)
+// deleteTodoListFromStorage("");
