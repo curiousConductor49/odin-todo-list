f6225be473e040f67e77ca19bcd0f50a12a8423b
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-28 16:07:12 -0400

    feat: update ids to classes for todo item html function, add buttons for todo list html function

--- src/crud-ops/displayTodoDataFromStorage.js
+++ src/crud-ops/displayTodoDataFromStorage.js
@@ -4,12 +4,12 @@ export function createTodoItemElement(todoItemData) {
     // create html
     const todoItemEl = `
         <div class="todo-item" data-id="${id}">
-            <p id="item-title">${title}</p>
-            <time id="item-due-date" datetime="${dueDate}">${new Date(dueDate).toDateString()}</time>
-            <p id="item-priority">${priority}</p>
+            <p class="item-title">${title}</p>
+            <time class="item-due-date" datetime="${dueDate}">${new Date(dueDate).toDateString()}</time>
+            <p class="item-priority">${priority}</p>
             <div class="item-btn-container">
-                <button id="update-item-btn">Update</button>
-                <button id="delete-item-btn">Delete</button>
+                <button class="update-item-btn">Update</button>
+                <button class="delete-item-btn">Delete</button>
             </div>
         </div>
     `
@@ -24,6 +24,10 @@ export function createTodoListElement(todoListData) {
         <div class="todo-list" data-id="${id}">
             <p class="list-title">${title}</p>
             <p class="list-description">${description}</p>
+            <div class="item-btn-container">
+                <button class="update-item-btn">Update</button>
+                <button class="delete-item-btn">Delete</button>
+            </div>
         </div>
     `
     return todoListEl;
