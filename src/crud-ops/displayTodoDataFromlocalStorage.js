ff4d7e04c9be6a022f4ebd3591d0957339b32e93
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-28 15:48:09 -0400

    feat: complete function to create html for a todo item

--- src/crud-ops/displayTodoDataFromlocalStorage.js
+++ src/crud-ops/displayTodoDataFromlocalStorage.js
@@ -1,5 +1,18 @@
-export function createTodoItemElement() {
-    
+export function createTodoItemElement(todoItemData) {
+    // destructure todo item data
+    const [ title, id, , dueDate, priority, ...rest] = todoItemData;
+    const todoItemEl = `
+        <div class="todo-item" data-id="${id}">
+            <p id="item-title">${title}</p>
+            <time id="item-due-date" datetime="${dueDate}">${new Date(dueDate).toDateString()}</time>
+            <p id="item-priority">${priority}</p>
+            <div class="item-btn-container">
+                <button id="update-item-btn">Update</button>
+                <button id="delete-item-btn">Delete</button>
+            </div>
+        </div>
+    `
+    return todoItemEl;
 }
 
 export function createTodoListElement() {}
