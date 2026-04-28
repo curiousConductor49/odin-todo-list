cd44be0c2518fbdd84dc710ea2c6ac8b4f7d0c91
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-28 15:07:43 -0400

    feat: add empty function exports for module to display todo data

---
+++ src/crud-ops/displayTodoDataFromlocalStorage.js
@@ -0,0 +1,8 @@
+export function createTodoItemElement() {
+    
+}
+
+export function createTodoListElement() {}
+export function displaySelectedTodoList() {}
+export function displayAllTodoItems() {}
+export function displayAllTodoLists() {}
