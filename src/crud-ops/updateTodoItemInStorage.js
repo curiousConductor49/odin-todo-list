e16a3dc0b62976c3ba4d8beb860cdf269a2de2c5
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-30 17:03:31 -0400

    feat: update todo item's parent list id when passed form data

--- src/crud-ops/updateTodoItemInStorage.js
+++ src/crud-ops/updateTodoItemInStorage.js
@@ -4,12 +4,14 @@ export default function updateTodoItemInStorage(inputData, todoItemId) {
         const [ title, description, dueDate, priority, parentList ] = inputData;
         // get JSON data for todo item
         const appData = JSON.parse(localStorage.getItem("todoAppData"));
+        const todoLists = appData["todoLists"];
         const todoItem = appData["todoItems"].find(item => item.id === todoItemId);
         // set properties of todo item to new values
         todoItem["title"] = title;
         todoItem["description"] = description;
         todoItem["dueDate"] = dueDate;
         todoItem["priority"] = priority;
+        todoItem["parentListId"] = todoLists.find(list => list.title === parentList).id;
         todoItem["parentListTitle"] = parentList;
         // set updated JSON data
         localStorage.setItem("todoAppData", JSON.stringify(appData));
