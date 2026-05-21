a5c5f68011eeab110a3d388913bb2baffc7aa680
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-05-21 13:17:12 -0400

    feat: set option values to be todo list ids instead of titles

--- src/utility/dynamicHtmlPopulation.js
+++ src/utility/dynamicHtmlPopulation.js
@@ -5,7 +5,7 @@ export function populateTodoListDropdown() {
         const todoLists = appData["todoLists"];
         // create option elements
         const blankOptionHTML = [`<option value="" disabled selected hidden>Select a list</option>`]
-        const optionsHTML = blankOptionHTML.concat(todoLists.map(list => `<option value="${list.title}">${list.title}</option>`).join(""));
+        const optionsHTML = blankOptionHTML.concat(todoLists.map(list => `<option value="${list.id}">${list.title}</option>`).join(""));
         return optionsHTML;
     } catch (error) {
         console.log("Error:", error);
