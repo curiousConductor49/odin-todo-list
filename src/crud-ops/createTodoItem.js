108bc6ada46bc337f8b08a1eae580731383b0faa
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-05-21 16:11:42 -0400

    fix: update how the parent list id of a new todo item is determined

--- src/crud-ops/createTodoItem.js
+++ src/crud-ops/createTodoItem.js
@@ -3,7 +3,7 @@ export default function createTodoItem(inputData) {
     const [ title, description, dueDate, priority, parentList ] = inputData;
     // get JSON data for todo item's parent list id
     const todoLists = JSON.parse(localStorage.getItem("todoAppData"))["todoLists"];
-    const parentTodoListId = todoLists.find(list => list.title === parentList)["id"];
+    const parentTodoListId = todoLists.find(list => list.id === parentList)["id"];
     // create and return todo item object
     const todoItem = {
         title,
